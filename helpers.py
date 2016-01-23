181bf0626687ded2d157a2724de5cad477ade2e5
masterofpuppets103 <masterofpuppets103@users.noreply.github.com>
2016-01-23 01:28:29 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
+#This file is used for all functions we need in more than one file, so we can reuse code
 import musicpd
 import helpers
 
-def printcurrentsong(client):
+def printcurrentsong(client):#gives some information to the song currently played
     timeplayed = float(client.status()['elapsed'])
     sekunden = round(timeplayed % 60)
     minuten = round(timeplayed /60)
